09b49b232693cc2e03d3c3c40188b711a2ec4940
Jont Olof Lyttkens <jontolof@centcode.com>
2011-08-29 20:50:37 +0200

    Initial CCCarouselView files added

---
+++ CCCarouselView/CCCarouselView.h
@@ -0,0 +1,16 @@
+//
+//  CCCarouselView.h
+//  CCCarouselView
+//
+//  Created by Jont Olof Lyttkens on 2011-08-29.
+//  Copyright 2011 Mobile Strategist and iOS Developer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CCCarouselView : UIScrollView {
+    
+}
+
+@end
