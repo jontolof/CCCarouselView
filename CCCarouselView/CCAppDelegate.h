bbcb8a09501e19d6ce94e3daebf0d64481550170
Jont Olof Lyttkens <jontolof@centcode.com>
2011-08-29 07:32:26 +0200

    First commit

---
+++ CCCarouselView/CCAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  CCAppDelegate.h
+//  CCCarouselView
+//
+//  Created by Jont Olof Lyttkens on 2011-08-26.
+//  Copyright 2011 Mobile Strategist and iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ CCCarouselView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CCCarouselView
+//
+//  Created by Jont Olof Lyttkens on 2011-08-26.
+//  Copyright 2011 Mobile Strategist and iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
