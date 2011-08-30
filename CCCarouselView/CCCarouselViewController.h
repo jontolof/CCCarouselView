af5016b23d43e846d3bb5257beb6d57a10350bb1
Jont Olof Lyttkens <jontolof.lyttkens@bontouch.com>
2011-08-30 17:05:03 +0200

    Adding CCCarouselViewController files.

---
+++ CCCarouselView/CCCarouselViewController.h
@@ -0,0 +1,16 @@
+//
+//  CCCarouselViewController.h
+//  CCCarouselView
+//
+//  Created by Jont Olof Lyttkens on 2011-08-30.
+//  Copyright 2011 Mobile Strategist and iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CCCarouselViewController : UIViewController {
+    
+}
+
+@end
