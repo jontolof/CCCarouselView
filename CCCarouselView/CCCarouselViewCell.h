74d034044c624934d9a4351e0bec43aa6fc7d96e
Jont Olof Lyttkens <jontolof.lyttkens@bontouch.com>
2011-08-30 19:42:33 +0200

    Adding CCCarouselViewCell

---
+++ CCCarouselView/CCCarouselViewCell.h
@@ -0,0 +1,16 @@
+//
+//  CCCarouselViewCell.h
+//  CCCarouselView
+//
+//  Created by Jont Olof Lyttkens on 2011-08-30.
+//  Copyright 2011 Mobile Strategist and iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CCCarouselViewCell : UIView {
+    
+}
+
+@end
---
+++ CCCarouselView/CCCarouselViewCell.m
@@ -0,0 +1,37 @@
+//
+//  CCCarouselViewCell.m
+//  CCCarouselView
+//
+//  Created by Jont Olof Lyttkens on 2011-08-30.
+//  Copyright 2011 Mobile Strategist and iOS Developer. All rights reserved.
+//
+
+#import "CCCarouselViewCell.h"
+
+
+@implementation CCCarouselViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
